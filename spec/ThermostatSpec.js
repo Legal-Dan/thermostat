2913c7d33cbeca124fb0013d0e2ce4fb8ec2bbed
Daniel Clements <clemies2000@hotmail.com>
2021-09-28 15:27:04 +0100

    Add functionality to increase or decrease temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -4,4 +4,17 @@ describe("Thermostat", function() {
     let thermostat = new Thermostat();
     expect(thermostat.temperature).toEqual(20);
    })
+
+   it("should be possible to increase the temperature", function() {
+    let thermostat = new Thermostat();
+    thermostat.increaseTemp(5)
+    expect(thermostat.temperature).toEqual(25);
+   })
+
+   it("should be possible to decrease the temperature", function() {
+    let thermostat = new Thermostat();
+    thermostat.decreaseTemp(5)
+    expect(thermostat.temperature).toEqual(15);
+   })
+
   });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,4 +2,12 @@ class Thermostat {
   constructor (temperature) {
       this.temperature = 20;
   }
+
+  increaseTemp(increase) {
+    this.temperature += increase
+  }
+
+  decreaseTemp(decrease) {
+    this.temperature -= decrease
+  }
 };
